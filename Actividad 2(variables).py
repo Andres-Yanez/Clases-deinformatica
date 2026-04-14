ebe8a2860a234c22d641808148c0baf66970d390
Andres Yanez <e11473@ism.edu.ec>
2026-04-14 12:37:15 -0500

    Actu 1 (var)

---
+++ Actividad 2(variables).py
@@ -0,0 +1,10 @@
+#Actividad 2 (variables)
+print(len("Andres Yanez"))
+first_name="Andres"
+print("first_name:",len(first_name))
+longitud=len(first_name)
+print(type(first_name))
+print(type(longitud))
+#
+primer_nombre, primer_apellido, pais, edad, estado_civil = "Andres", "Yanez", "Ecuador", 17, "Soltero"
+print( "Su pais es:",pais,"Su edad es:",edad)
