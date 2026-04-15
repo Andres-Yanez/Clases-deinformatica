d73aa2f20a7f742ea5090a4d06bfd3d3df07781a
Andres Yanez <e11473@ism.edu.ec>
2026-04-15 08:37:36 -0500

    Ejercicio 2

--- Actividad 2(variables).py
+++ Actividad 2(variables).py
@@ -8,3 +8,54 @@ print(type(longitud))
 #
 primer_nombre, primer_apellido, pais, edad, estado_civil = "Andres", "Yanez", "Ecuador", 17, "Soltero"
 print( "Su pais es:",pais,"Su edad es:",edad)
+#Ejercicio 1
+#Andres Yanez
+#15/04/2026
+#Primer programa en Python
+#%%
+nombre ="Andres"
+apellido ="Yanez"
+nombre_completo ="Andres Yanez"
+pais="Ecuador"
+ciudad="Quito"
+edad=17
+anio=2026
+estado_civil="Soltero"
+es_verdadero = True
+luz_encendida = False
+colegio, menor_de_edad, altura= "ISM NORTH", "Si", 1.70
+print(type(nombre),type(apellido),type(nombre_completo),type(pais),type(ciudad),type(edad),type(anio),type(estado_civil),type(es_verdadero),type(luz_encendida),type(colegio),type(menor_de_edad),type(altura))
+print(len(nombre))
+print(len(nombre_completo))
+#%%
+num1= 5
+num2= 4
+print(num1+num2)
+total = num1 + num2
+print(num1 - num2)
+diferencia = num1 - num2
+print(num1 * num2)
+producto = num1 * num2
+print(num1 / num2)
+division = num1 / num2
+print(num1 % num2)
+residuo = num1 % num2
+print(num1 ** num2)
+potencia = num1 ** num2
+print(num1 // num2)
+division_entera = num1 // num2
+#Circulo
+#%%
+radio = 30
+pi = 3.14
+area_circulo = pi * radio ** 2
+circuferencia_circulo = 2 * pi * radio
+# %%
+usuario= radio
+entrada = area_circulo
+name = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+edad = input("¿Cuál es tu edad? ")
+pais = input("¿Cuál es tu país? ")
+
+
